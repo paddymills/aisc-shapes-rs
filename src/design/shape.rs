6bc80edbe55c183ef1c4fef5370eb20a36e32f7f
paddymills <paddymills@pm.me>
2022-06-08 11:36:20 -0400

    split detail and design api

---
+++ src/design/shape.rs
@@ -0,0 +1,12 @@
+
+pub struct Shape {
+    pub shape: String,
+    pub edi_nomenclature: String,
+    pub aisc_label: String,
+}
+
+impl Shape {
+    pub fn new() -> i32 {
+        1i32
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,42 @@
+
+
+pub mod units {
+    // stolen from: https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=4ee51938ace195537df24613b3c54564
+    use std::sync::atomic::{AtomicBool, Ordering};
+
+    static METRIC: AtomicBool = AtomicBool::new(false);
+
+    pub fn metric() {
+        METRIC.store(true, Ordering::Relaxed);
+    }
+    
+    pub fn english() {
+        METRIC.store(false, Ordering::Relaxed);
+    }
+}
+
+
+#[cfg_attr(feature = "design", path="design/shape.rs")]
+mod shape;
+
+pub use shape::Shape;
+
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    #[cfg(feature="design")]
+    fn new_design() {
+        let result = Shape::new();
+        assert_eq!(result, 1);
+    }
+    
+    #[test]
+    #[cfg(not(feature="design"))]
+    fn new_design() {
+        let result = Shape::new();
+        assert_eq!(result, 2);
     }
 }
